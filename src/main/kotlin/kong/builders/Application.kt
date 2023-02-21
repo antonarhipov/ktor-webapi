523bdb7d0b9a1b8797a3e88923b647012b8f50ff
anton <ant.arhipov@gmail.com>
2023-02-21 23:58:13 +0200

    Initial commit

---
+++ src/main/kotlin/kong/builders/Application.kt
@@ -0,0 +1,14 @@
+package kong.builders
+
+import io.ktor.server.application.*
+import kong.builders.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSockets()
+    configureSerialization()
+    configureRouting()
+}
